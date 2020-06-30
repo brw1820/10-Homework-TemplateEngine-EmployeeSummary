f48c6b2050228fa154fb0346187366d39fde8d39
unknown <brw1820@gmail.com>
2020-06-29 20:00:08 -0400

    beginning build user prompt

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,55 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the role of your team member?",
+        choices: ["Employee", "Intern", "Engineer", "Manager"]
+      }]).then(function(choice) {
+          switch (choice.role)  {
+            case "Employee":
+                newEmployee();
+                break;
+            case "Intern":
+                newIntern()
+                break;
+            case "Engineer":
+                 newEngineer();
+                break;
+
+          }
+        });
+      }
+      function newEngineer(){
+          return inquirer.prompt(
+      {
+        type: "input",
+        name: "name",
+        message: "What is your team member's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your team member's ID number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your team member's E-Mail address?",
+      },
+      
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your team member's GitHub Username",
+      },
+);
+};
+
+promptUser();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
